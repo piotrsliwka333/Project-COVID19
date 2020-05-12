29dda8113b175ca7f19e1101e1ef1a27538a3390
Piotr Sliwka <piotrsliwka333@gmail.com>
2020-05-12 15:31:31 +0200

    finish HomePage section

--- src/js/Components/HomePage/AppointmentForm/AppointmentForm.js
+++ src/js/Components/HomePage/AppointmentForm/AppointmentForm.js
@@ -2,6 +2,7 @@ import React, {useEffect, useRef, useState} from "react";
 import lottie from "lottie-web";
 
 export const AppointmentForm = () => {
+	const API_URL = 'http://localhost:3011';
 	const [personDetails, setPersonDetails] = useState({
 		name: "",
 		surname: "",
@@ -10,14 +11,18 @@ export const AppointmentForm = () => {
 		city: ""
 	})
 
-	const [errors,setErrors] = useState(false)
+	const [errors,setErrors] = useState([])
+	const [status,setStatus] = useState('unActive')
+	const [inputEdit,setInputEdit] = useState(false);
 
 	const checkSubmit = (e) => {
+		setStatus("active")
 		e.preventDefault();
 		let errorArray = [];
 
 		if(personDetails.name.length < 2) {
 			errorArray.push("Too Short Name")
+
 		} if(personDetails.surname.length < 2) {
 			errorArray.push("Too short Surname")
 		} if(personDetails.phone < 9 || isNaN(parseFloat(personDetails.phone)) ) {
@@ -31,7 +36,39 @@ export const AppointmentForm = () => {
 		setErrors(errorArray)
 		if(errorArray.length === 0) {
 
+			const dataToSend = {
+				name: personDetails.name,
+				surname: personDetails.surname,
+				phone: personDetails.phone,
+				email: personDetails.email,
+				city: personDetails.city
+			}
 
+			fetch(`${API_URL}/people`,{
+				method: 'POST',
+				body: JSON.stringify(dataToSend),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			})
+				.then(response => response.json())
+				.then(data => {
+					setPersonDetails(prevState => {
+						return {
+							...prevState,
+							name: "",
+							surname: "",
+							phone: "",
+							email: "",
+							city: "",
+						}
+					})
+					setInputEdit(true)
+				})
+
+
+
+				.catch(error => console.log(error))
 
 		}
 	}
@@ -47,9 +84,16 @@ export const AppointmentForm = () => {
 		})
 	},[])
 
-
-
-
+	const formSent = useRef(null)
+	useEffect(()=> {
+		lottie.loadAnimation({
+			container: formSent.current,
+			renderer: 'svg',
+			loop: false,
+			autoplay: true,
+			animationData: require('../../../JSON/formSend.json')
+		})
+	},[])
 
 		const handleChange = (e) => {
 			const {name, value} = e.target;
@@ -61,28 +105,25 @@ export const AppointmentForm = () => {
 			});
 		};
 
-
-
-
+	console.log(errors.length)
 	return (
 		<article id='appointment' className="appointment col-10 col-md-6 col-xl-7">
 			<h3 className="appointment__title">Make an Appointment</h3>
 			<div className="appointment__box">
-				<form className="appointment-form">
+				<form aria-disabled={true} onSubmit={checkSubmit} className="appointment-form">
 					<label className="appointment-form__label">Name</label>
-					<input name="name" onChange={handleChange} value={personDetails.name} className="appointment-form__input"/>
-					<label className="appointment-form__label">Surname</label>
-					<input  name="surname" onChange={handleChange} value={personDetails.surname} className="appointment-form__input"/>
+					<input disabled={inputEdit} name="name" onChange={handleChange} value={personDetails.name} className="appointment-form__input"/>
+					<label  className="appointment-form__label">Surname</label>
+					<input  disabled={inputEdit} name="surname" onChange={handleChange} value={personDetails.surname} className="appointment-form__input"/>
 					<label className="appointment-form__label">Phone</label>
-					<input name="phone" onChange={handleChange} value={personDetails.phone} className="appointment-form__input"/>
+					<input disabled={inputEdit} name="phone" onChange={handleChange} value={personDetails.phone} className="appointment-form__input"/>
 					<label className="appointment-form__label">Email</label>
-					<input name="email" onChange={handleChange} value={personDetails.email} className="appointment-form__input"/>
+					<input disabled={inputEdit} name="email" onChange={handleChange} value={personDetails.email} className="appointment-form__input"/>
 					<label className="appointment-form__label">City</label>
-					<input name="city"  onChange={handleChange} value={personDetails.city} className="appointment-form__input"/>
-					<button className="appointment-form__btn">Make an Appointment</button>
+					<input disabled={inputEdit} name="city"  onChange={handleChange} value={personDetails.city} className="appointment-form__input"/>
+					<button disabled={inputEdit} type="submit" className="appointment-form__btn">Make an Appointment</button>
 				</form>
 				<div className="appointment-animation" ref={send}/>
-
 			</div>
 		</article>
 	)
